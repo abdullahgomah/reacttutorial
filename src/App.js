1d7789852f82696740dc3d5c194b5800fbbec56e
abdullahgomah <djangodeveloper6@gmail.com>
2024-08-14 19:37:02 +0300

    init

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          This is Abdullah Mohammed from React Tutorial 
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
